7b514da9d0fc9b5dae12d02264ca643b8408adf7
Joseph Udonsak <joseph.udonsak@live.co.uk>
2020-07-01 21:50:54 +0100

    First version of register route

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class APIController extends AbstractController
 {
     /**
-     * @Route("/a/p/i", name="a_p_i")
+     * @Route("/", name="api_index")
      */
     public function index()
     {
@@ -16,5 +19,54 @@ class APIController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/APIController.php',
         ]);
+    }
+
+    /**
+     * @Route("/register", name="register_user", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $firstName = $this->getParameterFromRequest('firstName', $request);
+        $middleName = $this->getParameterFromRequest('middleName', $request) ?? '';
+        $lastName = $this->getParameterFromRequest('lastName', $request);
+        $dateOfBirth = $this->getParameterFromRequest('dateOfBirth', $request);
+        $emailAddress = $this->getParameterFromRequest('emailAddress', $request);
+        $password = $this->getParameterFromRequest('password', $request);
+        //assuming first name, last name, email address and password are required
+
+        if (!$firstName) {
+            return $this->json([
+                'message' => 'firstName is required'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        if (!$lastName) {
+            return $this->json([
+                'message' => 'lastName is required'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        if (!$emailAddress) {
+            return $this->json([
+                'message' => 'emailAddress is required'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        if (!$password) {
+            return $this->json([
+                'message' => 'password is required'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        return $this->json([
+            'message' => 'User registered successfully',
+            'data' => [
+                'id' => uniqid('User_'),
+                'name' => "$firstName {$middleName} $lastName",
+            ]
+        ], Response::HTTP_CREATED);
+    }
+
+    private function getParameterFromRequest(string $parameterName, Request $request)
+    {
+        return $request->request->get($parameterName);
     }
 }
